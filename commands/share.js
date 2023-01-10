6f330d03dc3d93e471311dc90bd688c208c240eb
Kevin <kfnpfeffer@gmail.com>
2023-01-10 19:50:54 +0100

    Check if user is verified before sharing their workouts

--- commands/share.js
+++ commands/share.js
@@ -53,101 +53,109 @@ module.exports = {
   async execute(interaction) {
     console.log(interaction)
     const User = await getById(interaction.user.id)
-    if (interaction.options.getSubcommand() === 'latest') {
-      // should check if user is verified first
-      const workout = await getUserLatestWorkout(User.hevyUsername)
-
-      if (workout) {
-        const embeds = [embedWorkout(workout)]
-
-        await interaction.reply({
-          content: `<@${interaction.user.id}> latest workout.`,
-          embeds,
-          ephemeral: false,
-        })
-      } else {
-        await interaction.reply({
-          content: 'No latest workout found.',
-          ephemeral: true,
-        })
-      }
-    } else if (interaction.options.getSubcommand() === 'list') {
-      // should check if user is verified first
-      const workouts = await getUserWorkouts(User.hevyUsername, 1, 24)
-
-      const row = new ActionRowBuilder().addComponents(
-        new StringSelectMenuBuilder()
-          .setCustomId('workoutSelect')
-          .setPlaceholder('Select a workout')
-          .setMinValues(1)
-          .setMaxValues(1)
-          .addOptions(
-            workouts.map((w) => {
-              return {
-                label: w.name,
-                description: `${dayjs().to(dayjs(w.created_at))} - ${dayjs(
-                  w.created_at
-                ).format('llll')}`,
-                value: w.short_id,
-              }
-            })
-          )
-      )
-
-      const sentMessage = await interaction.reply({
-        content: 'Select a workout to share.',
-        components: [row],
-        ephemeral: true,
-      })
-      const collector = sentMessage.createMessageComponentCollector({
-        componentType: ComponentType.StringSelect,
-        time: 15000,
-      })
-
-      collector.on('collect', async (i) => {
-        if (i.customId === 'workoutSelect') {
-          const workoutShortId = i.values[0]
-
-          const associatedWorkout = workouts.find(
-            (w) => w.short_id === workoutShortId
-          )
-
-          const embeds = [embedWorkout(associatedWorkout)]
-          await interaction.deleteReply()
-          await interaction.channel.send({
-            content: `<@${interaction.user.id}> shared a workout.`,
-            embeds,
-            components: [],
-            ephemeral: false,
-          })
-        }
-      })
-    } else if (interaction.options.getSubcommand() === 'link') {
-      const workoutId = extractWorkoutId(
-        interaction.options.getString('link').trim()
-      )
-      if (workoutId) {
-        const workout = await getWorkoutById(workoutId)
+    if (User) {
+      if (interaction.options.getSubcommand() === 'latest') {
+        // should check if user is verified first
+        const workout = await getUserLatestWorkout(User.hevyUsername)
 
         if (workout) {
           const embeds = [embedWorkout(workout)]
 
-          await interaction.editReply({
+          await interaction.reply({
             content: `<@${interaction.user.id}> latest workout.`,
             embeds,
+            ephemeral: false,
           })
         } else {
-          await interaction.editReply({
-            content: `This workout doesn't seem to exist.`,
+          await interaction.reply({
+            content: 'No latest workout found.',
             ephemeral: true,
           })
         }
-      } else {
-        await interaction.editReply({
-          content: `Please provide a correct Hevy link.`,
+      } else if (interaction.options.getSubcommand() === 'list') {
+        // should check if user is verified first
+        const workouts = await getUserWorkouts(User.hevyUsername, 1, 24)
+
+        const row = new ActionRowBuilder().addComponents(
+          new StringSelectMenuBuilder()
+            .setCustomId('workoutSelect')
+            .setPlaceholder('Select a workout')
+            .setMinValues(1)
+            .setMaxValues(1)
+            .addOptions(
+              workouts.map((w) => {
+                return {
+                  label: w.name,
+                  description: `${dayjs().to(dayjs(w.created_at))} - ${dayjs(
+                    w.created_at
+                  ).format('llll')}`,
+                  value: w.short_id,
+                }
+              })
+            )
+        )
+
+        const sentMessage = await interaction.reply({
+          content: 'Select a workout to share.',
+          components: [row],
           ephemeral: true,
         })
+        const collector = sentMessage.createMessageComponentCollector({
+          componentType: ComponentType.StringSelect,
+          time: 15000,
+        })
+
+        collector.on('collect', async (i) => {
+          if (i.customId === 'workoutSelect') {
+            const workoutShortId = i.values[0]
+
+            const associatedWorkout = workouts.find(
+              (w) => w.short_id === workoutShortId
+            )
+
+            const embeds = [embedWorkout(associatedWorkout)]
+            await interaction.deleteReply()
+            await interaction.channel.send({
+              content: `<@${interaction.user.id}> shared a workout.`,
+              embeds,
+              components: [],
+              ephemeral: false,
+            })
+          }
+        })
+      } else if (interaction.options.getSubcommand() === 'link') {
+        const workoutId = extractWorkoutId(
+          interaction.options.getString('link').trim()
+        )
+        if (workoutId) {
+          const workout = await getWorkoutById(workoutId)
+
+          if (workout) {
+            const embeds = [embedWorkout(workout)]
+
+            await interaction.editReply({
+              content: `<@${interaction.user.id}> latest workout.`,
+              embeds,
+            })
+          } else {
+            await interaction.editReply({
+              content: `This workout doesn't seem to exist.`,
+              ephemeral: true,
+            })
+          }
+        } else {
+          await interaction.editReply({
+            content: `Please provide a correct Hevy link.`,
+            ephemeral: true,
+          })
+        }
       }
+    } else {
+      await interaction.reply({
+        content:
+          'You have not yet finished linking your Hevy account. Use `/link` and follow the instruction in order to share your workouts',
+        ephemeral: true,
+      })
     }
   },
 }
